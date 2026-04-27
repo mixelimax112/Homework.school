bc998f0e2821f7fac711a1ae410509d9f5c9c678
Maksym <mixelimaxkravyt@gmail.com>
2026-04-27 16:02:07 +0200

    homework 31.python

---
+++ PythonProject/hm31.py
@@ -0,0 +1,21 @@
+import re
+
+text = "The events N 123456 happened on 15/03/2025, 01.12.2024 and 09-09-2023. Deadline: 28/02/2022."
+
+
+res = re.findall(r"\d{2}/\d{2}/\d{4}|\d{2}\.\d{2}\.\d{4}|\d{2}-\d{2}-\d{4}", text)
+
+print(res)
+
+
+import re
+
+
+tag_input = "python, data-science / machine-learning; AI  neural-networks"
+
+
+tags = re.split(r'[ ,;/]+', tag_input)
+
+result = [tag.strip() for tag in tags if tag.strip()]
+
+print(result)
