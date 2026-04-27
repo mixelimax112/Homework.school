c13af8e983425571dca8c0a9fdc25602095c91ae
Maksym <mixelimaxkravyt@gmail.com>
2026-04-27 16:11:49 +0200

    homework 31.python

--- PythonProject/hm31.py
+++ PythonProject/hm31.py
@@ -3,9 +3,10 @@ import re
 text = "The events N 123456 happened on 15/03/2025, 01.12.2024 and 09-09-2023. Deadline: 28/02/2022."
 
 
-res = re.findall(r"\d{2}/\d{2}/\d{4}|\d{2}\.\d{2}\.\d{4}|\d{2}-\d{2}-\d{4}", text)
+result = re.finditer(r"\d{2}/\d{2}/\d{4}|\d{2}\.\d{2}\.\d{4}|\d{2}-\d{2}-\d{4}", text)
 
-print(res)
+for res in result:
+    print(res.group())
 
 
 import re
